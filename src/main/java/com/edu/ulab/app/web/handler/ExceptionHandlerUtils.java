68462c7715f0f3cdfa5ec428aae006ab0d2a1b4c
denvoiten <den.voiten@gmail.com>
2022-09-25 17:31:52 +0300

    initial commit

---
+++ src/main/java/com/edu/ulab/app/web/handler/ExceptionHandlerUtils.java
@@ -0,0 +1,21 @@
+package com.edu.ulab.app.web.handler;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+public final class ExceptionHandlerUtils {
+
+    private ExceptionHandlerUtils() {
+    }
+
+    public static String buildErrorMessage(Throwable t) {
+        StringBuilder message =
+                new StringBuilder(ExceptionUtils.getMessage(t));
+
+        Throwable cause = t.getCause();
+        if (cause != null) {
+            message.append(", cause: ").append(ExceptionUtils.getMessage(cause));
+        }
+
+        return message.toString();
+    }
+}
